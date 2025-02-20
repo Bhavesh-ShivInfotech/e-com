d8adc936298fb171d7c436f4440e130337b3ae06
Bhavesh <bhaveshpanchal.shivinfotech@gmail.com>
2025-02-20 17:57:08 +0530

    Changes-Final-New

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,32 +1,11 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import Login from "../pages/auth/Login";
-import ForgetPasswordPage from "../pages/auth/ForgetPassword";
 import Dashboard from "../pages/Dashboard";
-import Category from "../pages/Category/Category";
-import AddCategory from "../pages/Category/AddCategory";
-import EditCategory from "../pages/Category/EditCategory";
-import Product from "../pages/Product/Product";
-import AddProduct from "../pages/Product/AddProduct";
-import EditProduct from "../pages/Product/EditProduct";
-import ViewProduct from "../pages/Product/ViewProduct";
-import Report from "../pages/Report";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/forgot-password" element={<ForgetPasswordPage />} />
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/category" element={<Category />} />
-      <Route path="/addcategory" element={<AddCategory />} />
-      <Route path="/editcategory/:id" element={<EditCategory />} />
-      <Route path="/product" element={<Product />} />
-      <Route path="/addproduct" element={<AddProduct />} />
-      <Route path="/editProduct/:id" element={<EditProduct />} />
-      <Route path="/viewProduct/:id" element={<ViewProduct />} />
-      <Route path="/report" element={<Report />} />
     </Routes>
   );
 };
