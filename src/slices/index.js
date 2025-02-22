2684cd0b6295d149b07d249b938ebe0272ed2577
Bhavesh <bhaveshpanchal.shivinfotech@gmail.com>
2025-02-22 11:56:44 +0530

    Changes7

--- src/slices/index.js
+++ src/slices/index.js
@@ -1,12 +1,8 @@
 import { combineReducers } from "redux";
-
-// Front
 import LayoutReducer from "./layouts/reducer";
-// import ForgetPasswordReducer from "./auth/forgetpwd/reducer";
 
 const rootReducer = combineReducers({
   Layout: LayoutReducer,
-  // ForgetPassword: ForgetPasswordReducer,
 });
 
 export default rootReducer;
