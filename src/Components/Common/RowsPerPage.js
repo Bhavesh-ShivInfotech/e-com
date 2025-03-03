06fe5d39aab9e87203c183e0fabfc14286623a52
Bhavesh <bhaveshpanchal.shivinfotech@gmail.com>
2025-03-03 23:59:41 +0530

    Bug-Fix

--- src/Components/Common/RowsPerPage.js
+++ src/Components/Common/RowsPerPage.js
@@ -12,9 +12,11 @@ const RowsPerPage = ({ rowsPerPage, handleRowsPerPageChange }) => {
         value={rowsPerPage}
         onChange={handleRowsPerPageChange}
       >
-        <option value={10}>10</option>
-        <option value={20}>20</option>
+        <option value={5}>5</option>
+        <option value={15}>15</option>
+        <option value={25}>25</option>
         <option value={50}>50</option>
+        <option value={100}>100</option>
       </select>
     </div>
   );
